a1172739e336a1023667742e7b19c4de215039c1
Ryan Sy <ryanbsy@gmail.com>
2019-06-29 15:16:35 -0400

    added onfocus/onblur bill/percent inputs; a little styliing added

--- js/script.js
+++ js/script.js
@@ -1,54 +1,61 @@
 const bill = document.getElementById('bill');
 const percentage = document.getElementById('percentage');
 const split = document.getElementById('split');
-const splitYes = document.getElementById('splitYes');
-const splitNo = document.getElementById('splitNo');
-const splitContainer = document.getElementById('splitContainer');
 const tip = document.getElementById('tip');
 
-// toggle split field on and off - if off, disable input and set to 1
-splitYes.addEventListener('click', enableSplit);
-splitNo.addEventListener('click', disableSplit);
+// convert bill and percentage to currency strings on blur
+bill.onblur = toCurrencyString;
+percentage.onblur = toPercentageString;
+
+// convert bill and percentage to numbers on focus
+bill.onfocus = toCurrencyNumber;
+percentage.onfocus = toPercentageNumber;
 
 // delete tip value if bill, percentage or split are changed
 bill.addEventListener('change', updateTip);
 percentage.addEventListener('change', updateTip);
 split.addEventListener('change', updateTip);
 
-// event listener callback functions
-function enableSplit() {
-  split.removeAttribute('disabled');
-  tip.innerHTML = '';
-}
-
-function disableSplit() {
-  split.setAttribute('disabled', true);
-  split.value = 1;
-  tip.innerHTML = '';
-}
-
-function updateTip() {
-  tip.innerHTML = '';
-}
-
-// convert bill input value to currency string
-function convertToCurrencyString() {
-  if (isNaN(bill.value) || bill.value < 0) {
-    bill.value = '$0.00';
+function toCurrencyString() {
+  if (isNaN(bill.value) || bill.value < .01) {
+    bill.value = '';
   } else {
-    const n = new Number(bill.value);
+    const n = Number(bill.value);
     bill.value = n.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
   }
 }
 
-// convert percentage input value to percentage string
-function convertToPercentString() {
+function toPercentageString() {
   if (isNaN(percentage.value) || percentage.value < 1) {
-    percentage.value = '1%';
+    percentage.value = '';
   } else {
-    const n = new Number(percentage.value * .01);
+    const n = Number(percentage.value * .01);
     percentage.value = n.toLocaleString('en-US', { style: 'percent' });
   }
+}
+
+function toCurrencyNumber() {
+  let billValue = bill.value = Number(bill.value.replace(/[^\d.]/g, ''));
+
+  if (!billValue) {
+    bill.value = '';
+  } else {
+    bill.value = billValue;
+  }
+}
+
+function toPercentageNumber() {
+  let percentageValue = parseFloat(percentage.value);
+
+  if (!percentageValue) {
+    percentage.value = '';
+  } else {
+    percentage.value = percentageValue;
+  }
+}
+
+function updateTip() {
+  tip.innerHTML = '';
 }
 
 // main calculate tip function
@@ -56,14 +63,16 @@ function calculateTip() {
   const billAmount = Number(bill.value.replace(/[^\d.]/g, ''));
   const tipPercentage = parseFloat(percentage.value)/100;
   const total = (billAmount * tipPercentage / split.value);
-  const totalTip = new Number(total).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-
+  const totalTip = Number(total).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  let tipText = document.getElementById('tip-text');
   let text;
+
   if (split.value > 1) {
     text = ' per person';
   } else {
     text = '';
   }
 
-  tip.innerHTML = `<strong>${totalTip}</strong> ${text}`;
+  tip.innerHTML = `<strong>${totalTip}</strong>`;
+  tipText.innerHTML = `${text}`
 }
